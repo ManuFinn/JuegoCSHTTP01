cfc0fac8c71053e4d9d18175ee7b876e88eaaa54
Juan Jesús Chávez Villa <janitochvvll2311@gmail.com>
2021-11-17 14:09:13 -0600

    Libreria de clases del juego

---
+++ ClasesJuegoCS/Game/Player.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ClasesJuegoCS.Game
+{
+
+    public class Player
+    {
+
+        public string Name { get; set; }
+        public ulong Score { get; set; }
+
+    }
+
+}
